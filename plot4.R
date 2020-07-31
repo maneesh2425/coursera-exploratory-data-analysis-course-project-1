9f3700feb227aa924af034cf3171c16f6f68c496
nichanametla maneesh <61339153+maneesh2425@users.noreply.github.com>
2020-07-31 08:42:04 +0530

    fourth plot function

--- plot4.R
+++ plot4.R
@@ -1,29 +1,34 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#plot4 function
+#
+#Script with function
+#which make last four task plots
 
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
-voltage <- as.numeric(subSetData$Voltage)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-
-
-png("plot4.png", width=480, height=480)
-par(mfrow = c(2, 2)) 
-
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
-
-plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
-
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
-
-plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
-
-dev.off()
+plot4 <- function(task_data){
+  png(filename = "plot4.png", width = 480, height = 480)
+  
+  par(mfrow = c(2, 2))
+  
+  #first plot
+  plot(y = task_data$Global_active_power, x = task_data$Data_Time, type = "l", xlab = ""
+       , ylab = "Global Active Power (kilowatts)")
+  #second plot
+  plot(y = task_data$Global_active_power, x = task_data$Data_Time, type = "l", xlab = "datetime"
+       , ylab = "Voltage")
+  
+  #third plot
+  plot(y = task_data$Sub_metering_1, x = task_data$Data_Time, type = "l", xlab = ""
+       , ylab = "Energy sub metering", col = "black")
+  
+  points(y = task_data$Sub_metering_2, x = task_data$Data_Time, type = "l", col = "red")
+  
+  points(y = task_data$Sub_metering_3, x = task_data$Data_Time, type = "l", col = "blue")
+  
+  legend("topright",col = c("black", "red", "blue")
+         , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, )
+  
+  #fourth plot
+  plot(y = task_data$Global_reactive_power, x = data$Data_Time, type = "l", xlab = "datetime"
+       , ylab = "Global_reactive_power")
+  
+  dev.off()
+}
