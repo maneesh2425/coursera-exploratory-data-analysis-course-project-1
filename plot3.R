80f2ccdc8242656bd27977808e38ffab04efc5fb
nichanametla maneesh <61339153+maneesh2425@users.noreply.github.com>
2020-07-31 08:40:48 +0530

    function for third plot

--- plot3.R
+++ plot3.R
@@ -1,17 +1,20 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#plot3 function
+#
+#Script with function
+#which make third task plot
 
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-
-png("plot3.png", width=480, height=480)
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
-dev.off()
+plot3 <- function(task_data){
+  png(filename = "plot3.png", width = 480, height = 480)
+  
+  plot(y = task_data$Sub_metering_1, x = task_data$Data_Time, type = "l", xlab = ""
+       , ylab = "Energy sub metering", col = "black")
+  
+  points(y = task_data$Sub_metering_2, x = task_data$Data_Time, type = "l", col = "red")
+  
+  points(y = task_data$Sub_metering_3, x = task_data$Data_Time, type = "l", col = "blue")
+  
+  legend("topright",col = c("black", "red", "blue")
+         , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
+  
+  dev.off()
+}
